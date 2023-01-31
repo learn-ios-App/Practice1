19f9ef2a0537d2fea37f8638009e40b202bef398
Aoitoridayo <s2071150@s.konan-u.ac.jp>
2023-01-31 21:20:16 +0900

    Initial Commit

---
+++ Practice1/Practice1App.swift
@@ -0,0 +1,17 @@
+//
+//  Practice1App.swift
+//  Practice1
+//
+//  Created by 渡邊魁優 on 2023/01/31.
+//
+
+import SwiftUI
+
+@main
+struct Practice1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
